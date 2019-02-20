89b1fd18a139403e185e1d265df25508f7ea1c8a
Menik <menikdisney@gmail.com>
2019-02-20 15:36:43 +0600

    Implemeting Login with fakebackEnd

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,20 +1,33 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import 'rxjs/operator/map';
 
 @Injectable()
 export class AuthService {
   constructor(private http: Http) {
   }
 
-  login(credentials) { 
-   return this.http.post('/api/authenticate', 
-      JSON.stringify(credentials));
+  login(credentials) {
+    return this.http.post('/api/authenticate',
+      JSON.stringify(credentials))
+      .map(response => {
+        // console.log(response.json());
+
+        let result = response.json();
+
+        if (result && result.token) {
+          localStorage.setItem("token", result.token);
+          return true;
+        }
+
+        return false;
+      });
   }
 
-  logout() { 
+  logout() {
   }
 
-  isLoggedIn() { 
+  isLoggedIn() {
     return false;
   }
 }
