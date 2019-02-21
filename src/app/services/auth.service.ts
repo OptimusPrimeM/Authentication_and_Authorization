db293a3ee622799bdcac5acc9123b91d6b2f6323
Menik <menikdisney@gmail.com>
2019-02-21 15:41:51 +0600

    Imlement full and easy way

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/operator/map';
+import { JwtHelper, tokenNotExpired } from 'angular2-jwt';
 
 @Injectable()
 export class AuthService {
@@ -29,7 +30,25 @@ export class AuthService {
   }
 
   isLoggedIn() {
-    return false;
+    /*
+        let awtHelper = new JwtHelper();
+        let token = localStorage.getItem('token');
+    
+        console.log(`$token`);
+    
+        if (!token)
+          return false;
+    
+        let expirationDate = awtHelper.getTokenExpirationDate(token);
+        let isExpired = awtHelper.isTokenExpired(token);
+    
+        console.log(`Expration =  ${expirationDate} ,Is Expires = ${isExpired}`);
+    
+        return !isExpired;
+        */
+
+    // Easiest way to implement all of this code
+    return tokenNotExpired();
   }
 }
 
